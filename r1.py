46634a3bf87c8e211376b19b198809e8d4a20e2d
Julie Chiang <juliechiang0515@gmail.com>
2021-06-15 21:12:38 +0800

    one with r1.py

---
+++ r1.py
@@ -0,0 +1,34 @@
+#讀取檔案，可以使用continue
+def read_file(filename):
+    lines = [] 
+    with open(filename, 'r', encoding='utf-8-sig') as f:  #-sig為了去除uefef
+            for line in f:
+                lines.append(line.strip())
+    return lines
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person: #如果person有值!
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+main()
